ee4d02ed571fc457a12b8285efc020104d2213ac
Matthew Guizar <matthewrguizar@gmail.com>
2022-12-27 16:22:35 -0800

    adding shapes to an array

--- inheritance/Driver.js
+++ inheritance/Driver.js
@@ -4,8 +4,15 @@ var Shape_1 = require("./Shape");
 var Circle_1 = require("./Circle");
 var Rectangle_1 = require("./Rectangle");
 var myShape = new Shape_1.Shape(10, 15);
-console.log(myShape.getInfo());
 var myCircle = new Circle_1.Circle(5, 10, 15);
-console.log(myCircle.getInfo());
 var myRectangle = new Rectangle_1.Rectangle(0, 0, 3, 7);
-console.log(myRectangle.getInfo());
+//declare an array of shapes (empty)
+var theShapes = [];
+//add the shapes to the array
+theShapes.push(myShape);
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+for (var _i = 0, theShapes_1 = theShapes; _i < theShapes_1.length; _i++) {
+    var tempShape = theShapes_1[_i];
+    console.log(tempShape.getInfo());
+}
--- inheritance/Driver.ts
+++ inheritance/Driver.ts
@@ -3,12 +3,18 @@ import { Circle } from "./Circle";
 import { Rectangle } from "./Rectangle";
 
 let myShape = new Shape(10, 15);
-
-console.log(myShape.getInfo());
-
 let myCircle = new Circle(5,10,15);
-
-console.log(myCircle.getInfo());
-
 let myRectangle = new Rectangle(0,0,3,7);
-console.log(myRectangle.getInfo())
+
+//declare an array of shapes (empty)
+let theShapes: Shape[] = [];
+
+//add the shapes to the array
+
+theShapes.push(myShape);
+theShapes.push(myCircle);
+theShapes.push(myRectangle);
+
+for (let tempShape of theShapes){
+    console.log(tempShape.getInfo())
+}
